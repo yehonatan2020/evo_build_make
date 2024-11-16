a1da5b333e063f402fb45c0240f4f5ac79e16bf2
yehonatan2020 <yehonatan@yandex.ru>
2024-11-16 19:44:04 +0000

    Fix cherry picking mistake

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -22,29 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 
 # /product packages
 PRODUCT_PACKAGES += \
-<<<<<<< HEAD
-    Browser2 \
-    Calendar \
-    Camera2 \
-    Contacts \
-    DeskClock \
-    Gallery2 \
-    LatinIME \
-    Music \
-=======
->>>>>>> c2c38049aa (Remove those things we won't need)
     preinstalled-packages-platform-handheld-product.xml \
     QuickSearchBox \
     SettingsIntelligence \
     frameworks-base-overlays
 
-<<<<<<< HEAD
-=======
-ifeq ($(LINEAGE_BUILD),)
-PRODUCT_PACKAGES += \
-    LatinIME
-endif
-
->>>>>>> c2c38049aa (Remove those things we won't need)
 PRODUCT_PACKAGES_DEBUG += \
     frameworks-base-overlays-debug
